32fc13a0f8ac78e2645fbb35f0b92a151d428378
TUNG\Admin <tungpham.bh@gmail.com>
2018-12-24 11:08:52 +0700

    add voting app

---
+++ voting_app/public/js/app.js
@@ -0,0 +1,7 @@
+class ProductList extends React.Component {
+  render() {
+    return (
+      <div className='ui unstackable items'>Hello friends, I am a basic React Component</div>
+    );
+  }
+}
