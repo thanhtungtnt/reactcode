83b91f89f939cff21172dd124055d0f49983914d
Tung Pham <tungpham.bh@gmail.com>
2018-12-25 22:13:17 +0700

    update voting app - State

--- voting_app/public/js/app.js
+++ voting_app/public/js/app.js
@@ -1,9 +1,17 @@
 class ProductList extends React.Component {
+  handleProductUpvote(productID){
+    console.log(productID + ' was upvoted');
+  }
+
+  testProductList(votes){
+    console.log('this product has ' + votes + ' votes');
+  }
+
   render() {
-    const product = Seed.products[0];
-    return (
-      <div className='ui unstackable items'>
-        <Product 
+    const products = Seed.products.sort((a, b) => (b.votes - a.votes));
+    const productItems = products.map((product) => (
+      <Product 
+          key = {'product-'+product.id}
           id = {product.id} 
           title = {product.title}
           description = {product.description}
@@ -11,27 +19,48 @@ class ProductList extends React.Component {
           votes = {product.votes}
           submitterAvatarUrl = {product.submitterAvatarUrl}
           productImageUrl = {product.productImageUrl}
+          onVote = {this.handleProductUpvote}
         />
+    ));
+    return (
+      <div className='ui unstackable items'>
+        {productItems}
       </div>
     );
   }
 }
 
 class Product extends React.Component{
+  constructor(props) {
+    super(props);
+
+    this.handleUpVote = this.handleUpVote.bind(this);
+  }
+
+  handleUpVote(){
+    this.props.onVote(this.props.id);
+  }
+
   render() {
     return (
       <div className='item'>
         <div className='image'>
-          <img src='images/products/image-aqua.png' />
+          <img src={this.props.productImageUrl} />
         </div>
         <div className='middle aligned content'>
+        <div className='header'>
+            <a onClick={this.handleUpVote}>
+              <i className='large caret up icon' />
+            </a>
+            {this.props.votes}
+          </div>
           <div className='description'>
-            <a>Fort Knight</a>
-            <p>Authentic renaissance actors, delivered in just two weeks.</p>
+            <a href='{this.props.url}'>{this.props.title}</a>
+            <p>{this.props.description}</p>
           </div>
           <div className='extra'>
             <span>Submitted by:</span>
-            <img className='ui avatar image' src='images/avatars/daniel.jpg'
+            <img className='ui avatar image' src={this.props.submitterAvatarUrl}
             />
           </div>
         </div>
