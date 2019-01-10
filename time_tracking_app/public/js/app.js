9612c594a1ec0883bd33575456ec51269816dc2f
TUNG\Admin <tungpham.bh@gmail.com>
2019-01-10 16:26:33 +0700

    update time_tracking app

--- time_tracking_app/public/js/app.js
+++ time_tracking_app/public/js/app.js
@@ -5,6 +5,24 @@ class TimerDashboard extends React.Component {
     super(props);
   }
 
+  state = state = {
+    timers: [{
+            title: 'Practice squat',
+            project: 'Gym Chores',
+            id: uuid.v4(),
+            elapsed: 5456099,
+            runningSince: Date.now(),
+        },
+        {
+            title: 'Bake squash',
+            project: 'Kitchen Chores',
+            id: uuid.v4(),
+            elapsed: 1273998,
+            runningSince: null,
+        },
+    ],
+};
+
   //render EditableTimerList và ToogleTimerForm với tham số isOpen
   render() {
     return (
